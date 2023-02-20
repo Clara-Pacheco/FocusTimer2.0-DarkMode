d2c378245e9d1b87b9c4195e5264d8cad9f19d9b
Clara <clarapachecodev@gmail.com>
2023-02-20 12:31:39 -0300

    Implement logic for switching active card - changing background color + card sound

--- js/events.js
+++ js/events.js
@@ -15,15 +15,28 @@ export function Events({
   btnPlus,
   btnMinus,
   controls,
-  timer
+  timer,
+  rainCardPath,
+  forestCardPath,
+  cafeteriaCardPath,
+  fireplaceCardPath
 }){
 
   // Card events
 
   forestCard.addEventListener('click', () => {
     controls.toggleForestCard()
+    forestCardPath.style.fill ='#323238'
     if(bgForestCard.classList.contains('activeBg')){
       Sounds.soundForestStart()
+      forestCardPath.style.fill ='#fff'
+      rainCardPath.style.fill ='#323238'
+      cafeteriaCardPath.style.fill ='#323238'
+      fireplaceCardPath.style.fill ='#323238'
+      bgRainCard.classList.remove('activeBg')
+      bgCafeteriaCard.classList.remove('activeBg')
+      bgFireplaceCard.classList.remove('activeBg')
+      
     }else {
       Sounds.stopForestSound()
     }  
@@ -32,8 +45,17 @@ export function Events({
 
   rainCard.addEventListener('click', () => {
     controls.toggleRainCard()
+    rainCardPath.style.fill ='#323238'
     if(bgRainCard.classList.contains('activeBg')){
+      rainCardPath.style.fill ='#fff'
+      forestCardPath.style.fill ='#323238'
+      cafeteriaCardPath.style.fill ='#323238'
+      fireplaceCardPath.style.fill ='#323238'
       Sounds.soundRainStart()
+      bgForestCard.classList.remove('activeBg')
+      bgCafeteriaCard.classList.remove('activeBg')
+      bgFireplaceCard.classList.remove('activeBg')
+      
     }else{
       Sounds.stopRainSound()
     }
@@ -42,8 +64,16 @@ export function Events({
 
   cafeteriaCard.addEventListener('click', () => {
     controls.toggleCafeteriaCard()
+    cafeteriaCardPath.style.fill ='#323238'
     if(bgCafeteriaCard.classList.contains('activeBg')){
+      cafeteriaCardPath.style.fill ='#fff'
+      forestCardPath.style.fill ='#323238'
+      rainCardPath.style.fill ='#323238'
+      fireplaceCardPath.style.fill ='#323238'
       Sounds.soundCoffeeStart()
+      bgForestCard.classList.remove('activeBg')
+      bgRainCard.classList.remove('activeBg')
+      bgFireplaceCard.classList.remove('activeBg')
     }else {
       Sounds.stopCafeteriaSound()
     }
@@ -52,8 +82,17 @@ export function Events({
 
   fireplaceCard.addEventListener('click', () => {
     controls.toggleFireplaceCard()
+    fireplaceCardPath.style.fill ='#323238'
+   
     if(bgFireplaceCard.classList.contains('activeBg')){
+      fireplaceCardPath.style.fill ='#fff'
+      forestCardPath.style.fill ='#323238'
+      rainCardPath.style.fill ='#323238'
+      cafeteriaCardPath.style.fill ='#323238'
       Sounds.soundFireplaceStart()
+      bgForestCard.classList.remove('activeBg')
+      bgRainCard.classList.remove('activeBg')
+      bgCafeteriaCard.classList.remove('activeBg')
     }else {
       Sounds.stopFireplaceSound()
     }
