7f706b0377b0d7e46f57402a8b94cd7f14696dfd
Clara <clarapachecodev@gmail.com>
2023-02-20 11:50:31 -0300

    Create function to stop sounds, functions to start each sound and control volume and export them to m /

--- js/sounds.js
+++ js/sounds.js
@@ -1,41 +1,26 @@
-export function sounds () {
+export function sounds ({
+  inputForest,
+  inputRain,
+  inputCoffee,
+  inputFireplace
+}) {
   const cafeteriaSound = new Audio('assets/sounds/Cafeteria.wav')
   const rainSound = new Audio('assets/sounds/Chuva.wav')
   const forestSound = new Audio('assets/sounds/Floresta.wav')
   const fireplaceSound = new Audio('assets/sounds/Lareira.wav')
   const kitchenTimer = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true")
 
-  cafeteriaSound.loop = true
-  rainSound.loop = true
-  forestSound.loop = true
-  fireplaceSound.loop = true
-
-  function playCafeteriaSound(){
-    cafeteriaSound.play()
-  }
 
   function stopCafeteriaSound(){
     cafeteriaSound.pause()
-  }
-
-  function playRainSound(){
-    rainSound.play()
   }
 
   function stopRainSound(){
     rainSound.pause()
   }
 
-  function playForestSound(){
-    forestSound.play()
-  }
-
   function stopForestSound(){
     forestSound.pause()
-  }
-
-  function playFireplaceSound(){
-    fireplaceSound.play()
   }
 
   function stopFireplaceSound(){
@@ -46,16 +31,51 @@ export function sounds () {
     kitchenTimer.play()
    }
 
+   let soundsOff = (soundOne, soundTwo, soundThree) => {
+    soundOne.pause()
+    soundTwo.pause()
+    soundThree.pause()
+  }
+
+  const soundForestStart = () => {
+    soundsOff(rainSound, cafeteriaSound, fireplaceSound)
+    forestSound.play()
+    forestSound.volume = inputForest.value / 100
+    forestSound.loop = true
+  
+  }
+  
+  const soundRainStart = () => {
+    soundsOff(forestSound, cafeteriaSound, fireplaceSound)
+    rainSound.play()
+    rainSound.volume = inputRain.value / 100
+    rainSound.loop = true
+  }
+  
+  const soundCoffeeStart = () => {
+    soundsOff(rainSound, forestSound, fireplaceSound)
+    cafeteriaSound.play()
+    cafeteriaSound.volume = inputCoffee.value / 100
+    cafeteriaSound.loop = true
+  }
+  
+  const soundFireplaceStart = () => {
+    soundsOff(rainSound, forestSound, cafeteriaSound)
+    fireplaceSound.play()
+    fireplaceSound.volume = inputFireplace.value / 100
+    fireplaceSound.loop = true
+  }
+
   return {
-    playCafeteriaSound,
     stopCafeteriaSound,
-    playRainSound,
     stopRainSound,
-    playForestSound,
     stopForestSound,
-    playFireplaceSound,
     stopFireplaceSound,
-    timeOff
+    timeOff,
+    soundForestStart,
+    soundRainStart,
+    soundCoffeeStart,
+    soundFireplaceStart
   }
 
 
